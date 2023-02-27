a693c4e661f26125c0be1f190653a77eff0ef0b1
showyourwork <showyourwork>
2023-02-28 00:55:02 +0100

    [showyourwork] overleaf sync

--- src/scripts/plot-orb-grid.py
+++ src/scripts/plot-orb-grid.py
@@ -1,9 +1,9 @@
 import numpy as np
 
+#from scipy.interpolate import interp1d
+#import astropy.constants as c
 import astropy.units as u
 from astropy.time import Time
-
-import paths
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Arrow
@@ -35,6 +35,7 @@ def setup_plot(fig, i, oplane=False, losplane=False):
 
     ax = fig.add_subplot(4, 3, i, projection='3d')
     ax.set_proj_type('ortho')
+    ax.margins(-0.499)
     
     # plot orbit
     ax.plot3D(xyz[:,0], xyz[:,1], xyz[:,2], 'k-', lw=2)
@@ -82,8 +83,8 @@ def setup_plot(fig, i, oplane=False, losplane=False):
     # mark points
     ax.plot3D([0],[0],[0], 'C1*', label='Star', markersize=15)
     
-    ax.set_xlim(0.65,-0.65)
-    ax.set_ylim(-0.65,0.65)
+    ax.set_xlim(0.7,-0.7)
+    ax.set_ylim(-0.7,0.75)
     ax.set_zlim(-0.2,0.2)
     ax.axis('off')
     
@@ -93,7 +94,7 @@ def setup_plot(fig, i, oplane=False, losplane=False):
     
 xyz = xyz_position(time, params).to(u.au).value
 
-fig = plt.figure(figsize=(20,10))
+fig = plt.figure(figsize=(15,20)) #plt.figaspect(1)*2)
 
 index_an = np.argmin(abs(phase(time, params)-params['argument of periapsis'].to(u.rad).value/(2*np.pi)))
 pend = np.arctan2(xyz[index_an,1], -xyz[index_an,0])
@@ -108,7 +109,7 @@ inc = np.pi/2 - params['inclination'].to(u.rad).value
 ax.plot3D(di*np.cos(np.linspace(0,inc,100))*np.sin(pend)+xyz[index_an,0], di*np.cos(np.linspace(0,inc,100))*np.cos(pend)+xyz[index_an,1], di*np.sin(np.linspace(0,inc,100)), 'r-')
 ax.plot3D([xyz[index_an,0], 1.5*di*np.cos(inc)+xyz[index_an,0]],[xyz[index_an,1], xyz[index_an,1]], [0,1.5*di*np.sin(inc)], 'r--')
 ax.plot3D([xyz[index_an,0], xyz[index_an,0]+1.5*di*np.sin(pend)],[xyz[index_an,1], xyz[index_an,1]+1.5*di*np.cos(pend)], [0,0], 'r--')
-ax.text(xyz[index_an,0]+2*di,xyz[index_an,1]+0.4, di/4, '$90^\circ-i$', None, fontsize=20, va='center', ha='center')
+ax.text(xyz[index_an,0]+0.4,xyz[index_an,1]-0.4, -di/8, '$90^\circ-i$', None, fontsize=20, va='center', ha='center')
 
 # mark LOS arrow
 ax.plot3D([-0.65,0], [0, 0], [0, 0], 'k<-', label='Line of Sight (to Earth)', markersize=1, zorder=-10)
@@ -123,10 +124,10 @@ ax.plot3D([],[],[], 'k-<', label='Line of Sight (to Earth)')
 ax.plot3D([],[],[], 'ko', label='Mask', alpha=0.3)
 ax.plot([],[],[], ' ', label='Inclination')
 ax.legend(framealpha=0, loc=2, fontsize=16)
-ax.text2D(0.08,0.2, '$i$', transform=ax.transAxes, fontsize=18, fontweight='bold')
+ax.text2D(0.1,0.65, '$i$', transform=ax.transAxes, fontsize=18, fontweight='bold')
 
 # legend 2
-ax = fig.add_subplot(4, 3, 3)
+ax = fig.add_subplot(4, 3, 3, projection='3d')
 ax.axis('off')
 ax.scatter([],[], c='#5790fc', marker='o', label=r'$\phi = 90\degree$')
 ax.scatter([],[], c='#964a8b', marker='o', label=r'$\phi_{max}$')
@@ -135,12 +136,12 @@ ax.legend(framealpha=0, loc=2, fontsize=16)
 
 ### inc 1: mask 1 ###
 ax = setup_plot(fig, 4)
-ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=45, alpha=0.3)
+ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=75, alpha=0.3)
 ax.view_init(elev=0, azim=0)
 
 ### inc 1: mask 2 ###
 ax = setup_plot(fig, 5)
-ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=65, alpha=0.3)
+ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=150, alpha=0.3)
 ax.plot3D([xyz[500,0]], [xyz[500,1]], [xyz[500,2]], c='#5790fc', marker='o', markersize=10)
 ax.plot3D([xyz[250,0]], [xyz[250,1]], [xyz[250,2]], c='#7a21dd', marker='o', markersize=10)
 ax.plot3D([xyz[750,0]], [xyz[750,1]], [xyz[750,2]], c='#964a8b', marker='o', markersize=10)
@@ -148,54 +149,54 @@ ax.view_init(elev=0, azim=0)
 
 ### inc 1: mask 3 ###
 ax = setup_plot(fig, 6)
-ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=100, alpha=0.3)
+ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=200, alpha=0.3)
 ax.view_init(elev=0, azim=0)
 
 ### inc 2: mask 1 ###
 ax = setup_plot(fig, 7)
-ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=45, alpha=0.3)
+ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=75, alpha=0.3)
 ax.view_init(elev=-20, azim=0)
 
-iaxes = inset_axes(ax,width=1,height=3,bbox_transform=ax.transAxes,bbox_to_anchor=(-0.1,0.5),loc=6)
+iaxes = inset_axes(ax,width=1,height=3,bbox_transform=ax.transAxes,bbox_to_anchor=(-0.2,0.5),loc=6)
 iaxes.add_patch(Arrow(0.5,0.85,0,-0.7, width=0.6, color='k'))
 iaxes.annotate('Inclination', (0.3, 0.55), color='k', fontsize=16, ha='center', va='center', rotation=90)
 iaxes.axis('off')
 
 ### inc 2: mask 2 ###
 ax = setup_plot(fig, 8)
-ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=65, alpha=0.3)
+ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=150, alpha=0.3)
 ax.plot3D([xyz[500,0]], [xyz[500,1]], [xyz[500,2]], c='#5790fc', marker='o', markersize=10)
-ax.plot3D([xyz[280,0]], [xyz[280,1]], [xyz[280,2]], c='#7a21dd', marker='o', markersize=10)
-ax.plot3D([xyz[720,0]], [xyz[720,1]], [xyz[720,2]], c='#964a8b', marker='o', markersize=10)
+ax.plot3D([xyz[350,0]], [xyz[350,1]], [xyz[350,2]], c='#7a21dd', marker='o', markersize=10)
+ax.plot3D([xyz[650,0]], [xyz[650,1]], [xyz[650,2]], c='#964a8b', marker='o', markersize=10)
 ax.view_init(elev=-20, azim=0)
 
 ### inc 2: mask 3 ###
 ax = setup_plot(fig, 9)
-ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=100, alpha=0.3)
+ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=200, alpha=0.3)
 ax.view_init(elev=-20, azim=0)
 
 ### inc 3: mask 1 ###
 ax = setup_plot(fig, 10)
-ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=45, alpha=0.3)
+ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=75, alpha=0.3)
 ax.view_init(elev=-40, azim=0)
 
 ### inc 3: mask 2 ###
 ax = setup_plot(fig, 11)
-ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=65, alpha=0.3)
+ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=150, alpha=0.3)
 ax.plot3D([xyz[500,0]], [xyz[500,1]], [xyz[500,2]], c='#5790fc', marker='o', markersize=10)
-ax.plot3D([xyz[290,0]], [xyz[290,1]], [xyz[290,2]], c='#7a21dd', marker='o', markersize=10)
-ax.plot3D([xyz[710,0]], [xyz[710,1]], [xyz[710,2]], c='#964a8b', marker='o', markersize=10)
+ax.plot3D([xyz[360,0]], [xyz[360,1]], [xyz[360,2]], c='#7a21dd', marker='o', markersize=10)
+ax.plot3D([xyz[640,0]], [xyz[640,1]], [xyz[640,2]], c='#964a8b', marker='o', markersize=10)
 ax.view_init(elev=-40, azim=0)
 
-iaxes = inset_axes(ax,width=3,height=1,bbox_transform=ax.transAxes,bbox_to_anchor=(0.5,-0.3),loc=8)
+iaxes = inset_axes(ax,width=3,height=1,bbox_transform=ax.transAxes,bbox_to_anchor=(0.5,0.0),loc=8)
 iaxes.add_patch(Arrow(0.15,0.5,0.7,0, width=0.6, color='k'))
 iaxes.annotate('Inner Working Angle', (0.5, 0.2), color='k', fontsize=16, ha='center', va='center')
 iaxes.axis('off')
 
 ### inc 3: mask 3 ###
 ax = setup_plot(fig, 12)
-ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=100, alpha=0.3)
+ax.plot3D([0],[0],[0], 'ko', label='Mask', markersize=200, alpha=0.3)
 ax.view_init(elev=-40, azim=0)
 
-plt.subplots_adjust(wspace=None, hspace=None)
-plt.savefig(paths.figures / 'orb-grid.pdf')
+plt.subplots_adjust(wspace=0.0, hspace=-0.4)
+plt.savefig('./orb-grid.pdf')
