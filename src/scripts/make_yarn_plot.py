22312bb958e41d8cbd55c1cbbe7e27905312a882
Timothy Gebhard <mail@timothygebhard.de>
2023-02-27 16:30:26 +0100

    Adjust code style to reduce complaints from linter

--- src/scripts/make_yarn_plot.py
+++ src/scripts/make_yarn_plot.py
@@ -8,9 +8,9 @@ from samplers import *
 np.random.seed(42) # set a seed so the plots and data output are reporducible for showyourwork!
 
 # this will make a plot with the number of orbits per panel and side of plots along one side
-plotit = 1 # set to 0 to skip plot generation
+plotit = 1  # set to 0 to skip plot generation
 norbs_plot = 10
-nstars_plot = 4 # you will make a plot with nstars_plot by nstars_plot panels
+nstars_plot = 4  # you will make a plot with nstars_plot by nstars_plot panels
 
 tyb = dict(color='black', fontsize=6)
 
@@ -28,20 +28,21 @@ nplanets = 10
 norbitsample = 1000
 
 # IWA ranges
-iwa = np.linspace(20,120,5) # inner working angles (in mas) to compute betamax at
+# inner working angles (in mas) to compute betamax at
+iwa = np.linspace(20, 120, 5)
 
 # read in the stars
 from astropy.io import ascii
 
 fname = "2646_NASA_ExEP_Target_List_HWO_Table.csv"
-t = ascii.read(paths.data / fname ,header_start = 1, data_start = 2)
+t = ascii.read(paths.data / fname, header_start=1, data_start=2)
 
 nstars = len(t)
 print(f'# read in {nstars} stars from {fname}')
 
 # set up output arrays
-betamin = np.zeros((nplanets,nstars,iwa.size))
-betamax = np.zeros((nplanets,nstars,iwa.size))
+betamin = np.zeros((nplanets, nstars, iwa.size))
+betamax = np.zeros((nplanets, nstars, iwa.size))
 
 if plotit:
 
@@ -70,7 +71,7 @@ for l in t: # loop over all the stars in the table
 	d = l['sy_dist']      		# parsecs
 	Vmag = l['sy_vmag']   		# V magnitude
 	mass = l['st_mass']   		# stellar mass
-	hz =  l['st_eei_angsep'] 	# Habitable zone distance (mas)
+	hz = l['st_eei_angsep'] 	# Habitable zone distance (mas)
 
 	if (plotit) and (cax < (nstars_plot*nstars_plot)):
 		ax[cax].set_xlim(-2*hz, 2*hz)
@@ -93,20 +94,20 @@ for l in t: # loop over all the stars in the table
 		#
 		P = 1.0 * u.year
 		tperi = 2050. * u.year
-		a = hz # in mas
+		a = hz  # in mas
 		e = esamp[n]
 		i = isamp[n]
 		w = wsamp[n]
 		anode = anodesamp[n]
 
-		if (plotit) and (n < norbs_plot) and (cax < (nstars_plot*nstars_plot)):
-			epochs = np.linspace(tperi, tperi+P, 100, endpoint='true')
+		if plotit and (n < norbs_plot) and (cax < (nstars_plot*nstars_plot)):
+			epochs = np.linspace(tperi, tperi+P, 100, endpoint=True)
 			# draw the orbit
-			_, _, Xorb, Yorb, Zorb, _, _, _ = kep3d(epochs,P,tperi,a,e,i,w,anode)
-			ax[cax].plot(Xorb,Yorb)
+			_, _, Xorb, Yorb, Zorb, _, _, _ = kep3d(epochs, P, tperi, a, e, i, w, anode)
+			ax[cax].plot(Xorb, Yorb)
 
 
-	cax+=1 # increase figure number
+	cax += 1 # increase figure number
 
 if plotit:
 	plt.draw()
