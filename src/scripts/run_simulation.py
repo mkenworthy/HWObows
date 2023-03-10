d9e814b5b5867f21d7148aa0b186c3f8ef5bc376
showyourwork <showyourwork>
2023-03-10 15:47:53 +0100

    [showyourwork] overleaf sync

--- src/scripts/run_simulation.py
+++ src/scripts/run_simulation.py
@@ -9,28 +9,10 @@ from tqdm import tqdm
 import time
 import paths
 
-#import sys 
-
-#if (len(sys.argv) != 2):
-#	print('need one command line argument (iwa, iwa2 or iwa3). Stopping...')
-#	quit()
-
 np.random.seed(42) # set a seed so the plots and data output are reporducible for showyourwork!
 
-# this will make a plot with the number of orbits per panel and side of plots along one side
-plotit = 0 # set to 0 to skip plot generation
-norbs_plot = 10
-nstars_plot = 4 # you will make a plot with nstars_plot by nstars_plot panels
-
-tyb = dict(color='black', fontsize=14)
-
-def draw_iwa(ax, iwa):
-	from matplotlib.patches import Circle
-	for r in iwa:
-		ax.add_patch(Circle((0,0),radius=r,alpha=0.2))
-
 # number of planet sims per star
-nplanets = 1000
+nplanets = 20
 
 # number of epochs to sample along the orbit of the planet
 norbitsample = 1000
@@ -64,18 +46,6 @@ print(f'# read in {nstars} stars from {fname}')
 betamin = np.zeros((nplanets,nstars,iwa.size))
 betamax = np.zeros((nplanets,nstars,iwa.size))
 
-if (plotit):
-	fig, axes = plt.subplots(nstars_plot, nstars_plot, figsize=(10,10))
-	ax = axes.flatten()
-	fig.suptitle(f'Inner Working Angles = {iwa} mas')
-	# remove the x and y ticks
-	for a in ax:
-		a.set_xticks([])
-		a.set_yticks([])
-		a.set_aspect('equal')
-
-	fig.subplots_adjust(wspace=0, hspace=0)
-
 cax = 0 # counter for the grid of subplots - this increases by 1 for each star AND also counts the star number for output array
 
 for l in tqdm(t): # loop over all the stars in the table
@@ -84,16 +54,6 @@ for l in tqdm(t): # loop over all the stars in the table
 	Vmag = l['sy_vmag']   		# V magnitude
 	mass = l['st_mass']   		# stellar mass
 	hz =  l['st_eei_angsep'] 	# Habitable zone distance (mas)
-
-#	print(f'# working on {star} {cax} of {nstars} at {d:.1f} parsecs with V={Vmag:.1f} and HZ radius={hz:.1f} mas')
-
-	if (plotit) and (cax < (nstars_plot*nstars_plot)):
-		ax[cax].set_xlim(-1.5*hz, 1.5*hz)
-		ax[cax].set_ylim(-1.5*hz, 1.5*hz)
-		draw_iwa(ax[cax],iwa)
-		ax[cax].text(0.05, 0.95, star, ha='left', va='top', transform=ax[cax].transAxes, fontfamily='bold', fontsize=18)
-		ax[cax].text(0.05, 0.05, f'HZ={hz:.1f} mas', ha='left', va='bottom', transform=ax[cax].transAxes, **tyb)
-
 
 	# draw e and i from these distributions
 	esamp = sample_e(nplanets)
@@ -115,12 +75,6 @@ for l in tqdm(t): # loop over all the stars in the table
 		i = isamp[n]
 		w = wsamp[n]
 		anode = anodesamp[n]
-
-		if (plotit) and (n < norbs_plot) and (cax < (nstars_plot*nstars_plot)):
-			epochs = np.linspace(tperi, tperi+P, 100, endpoint='true')
-			# draw the orbit
-			_, _, Xorb, Yorb, Zorb, _, _, _ = kep3d(epochs,P,tperi,a,e,i,w,anode)
-			ax[cax].plot(Xorb,Yorb)
 
 		epochs = np.linspace(tperi, tperi+P, norbitsample, endpoint='true')
 
@@ -153,18 +107,8 @@ for l in tqdm(t): # loop over all the stars in the table
 
 	cax+=1 # increase figure number
 
-if plotit:
-	plt.draw()
-	plt.savefig('ball_of_yarn.png',dpi=100)
-	plt.savefig('ball_of_yarn.pdf')
-	plt.show()
-else:
-	print(f'saving the data with command line option {op}')
-#	np.save(paths.data / f'iwa{tag}.npy',iwa)
-#	np.save(paths.data / f'betamax{tag}.npy',betamax)
-#	np.save(paths.data / f'betamin{tag}.npy',betamin)
-
-	np.savez_compressed(paths.data / f'iwa_all{tag}.npz', iwa=iwa, betamax=betamax, betamin=betamin)
+print(f'saving the data with command line option {op}')
+np.savez_compressed(paths.data / f'iwa_all{tag}.npz', iwa=iwa, betamax=betamax, betamin=betamin)
 
 
 	
